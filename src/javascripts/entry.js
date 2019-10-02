a755f1fe27b84b3a3caf819b83f3b8df9c5fcac0
souh <ikasumi9999999@yahoo.co.jp>
2019-10-02 23:47:23 +0900

    add Bootstrap-vue

--- src/javascripts/entry.js
+++ src/javascripts/entry.js
@@ -1,7 +1,11 @@
 import '../css/base.scss';
 
 import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
 import App from './App.vue';
+
+// Bootstrap
+Vue.use(BootstrapVue);
 
 new Vue({
   el: '#app',
